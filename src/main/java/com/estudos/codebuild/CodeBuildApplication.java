e2c09f3444323a1ced97faf6e006a55bc64983bb
Eduardo Rodrigues <erodrigues@crdc.com.br>
2022-08-26 13:40:01 -0300

    Criação do Projeto

---
+++ src/main/java/com/estudos/codebuild/CodeBuildApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.codebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/codebuild/CodeBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.codebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
